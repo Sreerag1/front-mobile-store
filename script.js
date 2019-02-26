6b985b27f811ae72afb1216f9d95e6b66fe5f577
Sreerag1 <sreeragnair19.sn@gmail.com>
2019-02-26 21:17:34 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+  var mobileList;
+  loadMobiles();
+  function loadMobiles(argument) {
+      $.getJSON( "data.json", function( data ) {
+        data = data.mobiles;
+        mobileList = data;
+        $.each( data, function( key, val ) {
+                var card = '<div class="col-sm-6 col-md-4 col-lg-3">'+
+                  '<div class="thumbnail mobile-card" '+
+                  'data-manufacturer="'+val.manufacturer+'"'+
+                  'data-storage="'+val.storage+'"'+
+                  'data-os="'+val.os+'">'+
+                    '<a href="'+val.url+'" data-lightbox="'+val.url+'" data-title="'+val.name+'">'+
+                      '<div class="img-container">'+
+                        '<img class="img-responsive2" src="'+val.url+'" alt="Lights" style="">'+
+                      '</div>'+
+                    '</a>'+
+                    '<div class="mobile-info">'+
+                      '<h2>'+val.name+'</h2>'+
+                      '<p><strong>Manufacturer:</strong>'+val.manufacturer+'</p>'+
+                      '<p><strong>Storage:</strong>'+val.storage+'</p>'+
+                      '<p><strong>OS:</strong>'+val.os+'</p>'+
+                      '<h3 class="text-right price">Rs '+val.price+'</h3>'+
+                    '</div>'+
+                  '</div>'+
+                '</div>';
+          $("#mobiles-row").append(card);
+          
+          // end of loop
+        }); 
+        $(document).trigger('finished_loading');
+        // end of getJSON
+      });
+  }
+  $(document).on( "finished_loading", function() {
+    equalizeHeight();
+  });
+
+  $("#clear-filter").on( "click",clearFilters);
+  function clearFilters(argument) {
+      $(".mobile-card").css("opacity", 1);
+      $('#filter-container input[type=checkbox]').prop('checked', false);
+  }
+  $("#filter-container input[type=checkbox]").on( "click", function() {
+      $checkbox = $(this);
+      $(".mobile-card").css("opacity", .2);
+      $("#filter-container input[type=checkbox]:checked").each(function(index, element){
+        $("[data-"+this.name+"="+this.value+"]").css("opacity", 1);
+      });
+      if ($checkbox.is(":checked") == false) {
+        if ($("input[type=checkbox]:checked").length == 0) {
+          // if no checkbox is checked
+          clearFilters();
+        }
+        //  else {
+        //   // if hide the selected filter checkbox is unchecked
+        //    $("[data-"+this.name+"="+this.value+"]").css("opacity", .2);
+        //   console.log($checkbox.is(":checked"));          
+        // }
+      }
+
+  });
+  function equalizeHeight() {
+      var maxHeight = 0;
+      $(".mobile-card").each(function(){
+      if ($(this).height() > maxHeight) { maxHeight = $(this).height(); }
+      });
+      $(".mobile-card").height(maxHeight);          
+  }
+  // end of document ready
+});
